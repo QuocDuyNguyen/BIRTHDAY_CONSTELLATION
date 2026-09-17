33672af95167fb138d4243240860041d036ad9fb
Hakui <khanhukm2107@gmail.com>
2026-09-17 22:17:34 +0700

    adjust balloon positions around titles

--- src/components/Balloons.tsx
+++ src/components/Balloons.tsx
@@ -12,13 +12,13 @@ interface BalloonSpec {
 }
 
 const balloons: BalloonSpec[] = [
-  { color: "lilac", left: "3%", top: "25%", size: 82, delay: 0.05, duration: 1.05, rotate: -7 },
+  { color: "lilac", left: "3%", top: "2%", size: 82, delay: 0.05, duration: 1.05, rotate: -7 },
   { color: "blush", left: "10%", top: "56%", size: 68, delay: 0.28, duration: 1.2, rotate: 5 },
   { color: "cream", left: "18%", top: "34%", size: 52, delay: 0.48, duration: 1.1, rotate: -4 },
   { color: "berry", left: "73%", top: "40%", size: 74, delay: 0.18, duration: 1.16, rotate: 6 },
   { color: "lilac", left: "84%", top: "23%", size: 58, delay: 0.38, duration: 1.08, rotate: -5 },
   { color: "blush", left: "89%", top: "66%", size: 86, delay: 0.62, duration: 1.28, rotate: 7 },
-  { color: "cream", left: "65%", top: "72%", size: 44, delay: 0.76, duration: 1.22, rotate: -3 },
+  { color: "cream", left: "71%", top: "27%", size: 44, delay: 0.76, duration: 1.22, rotate: -3 },
 ];
 
 export function Balloons({ visible, paused = false }: { visible: boolean; paused?: boolean }) {
